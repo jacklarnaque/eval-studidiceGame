ea824bee7538e1a3fc196f9eaec27a674ae23c8e
jacklarnaque <grousset1065@live.fr>
2022-07-06 19:03:32 +0100

    Co-authored-by: TBDC <ThomasBDC@users.noreply.github.com>

--- scriptGamePage.js
+++ scriptGamePage.js
@@ -1,29 +1,78 @@
+
+// this is the dice loader (transition animation)
 setTimeout(function () {
-    const diceLoader = document.getElementById('centering');
-    diceLoader.hidden = "true";
+  const diceLoader = document.getElementById('centering');
+  diceLoader.hidden = "true";
 }, 1000);
-// function to pick a random number betwin 1 to 6
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-//insert in the dom element via js to display the score :
 
-/* let score1P = document.createElement('p');
-score1P.innerHTML = ${result};
-let score1 = document.getElementById('score1');
-score1.appendChild(score1P); */
+// the code below is to get the players names from the prompt of the pagePlay.html:
+const urlParams = new URLSearchParams(window.location.search);
+const getQueryParams = (params, url) => {
+  let href = url;
+  // this is an expression to get query strings
+  let regexp = new RegExp('[?&]' + params + '=([^&#]*)', 'i');
+  let qString = regexp.exec(href);
+  return qString ? qString[1] : null;
+};
 
-//algo dices game //
-let throwDices = document.getElementById("lancer")
-let playerTurnDiceButton = document.getElementById("throw")
+let player1 = getQueryParams('player1', window.location);
+let player2 = getQueryParams('player2', window.location);
+
+//the code below is to display the  global users scores
+document.getElementById('globalScore1').innerHTML = `score global ${player1} = `
+document.getElementById('globalScore2').innerHTML = `score global ${player2} = `
+document.getElementById('roundScore1').innerHTML = `Score Tour ${player1} = `
+document.getElementById('roundScore2').innerHTML = `Score Tour ${player2} = `
 
 
-playerTurnDiceButton.addEventListener('click', () => {
-    let n = getRandomInt(1, 7)
+//we begin the algo for the dice game
+
+const firstRandomNum = Math.floor(Math.random()
+  * 6) + 1
+
+// we now get the 
+// we need to get the variables necessary :
+let rollDice = document.getElementById('roll');
+let hold = document.getElementById('hold');
+let displayTurnPlayer = document.getElementById('displayTurnPlayer');
+
+
+let activePlayer, roundScore, globalScore, dice;
+
+function start() {
+  globalScore = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.getElementById('score1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+}
+
+//listners:
+window.addEventListener('load', (player1, player2) => {
+  displayTurnPlayer.innerHTML = `${player1} à votre tour de jouer!`
+  displayTurnPlayer.innerHTML = `${player2} à votre tour de jouer!`
+});
+
+
+const firstDiceImg = 'img/imgDice/dice-' + firstRandomNum + '.png'
+document.querySelectorAll('img')[0].setAttribute('src', firstDiceImg)
+
+
+let num = '';
+
+rollDice.addEventListener('click', () => {
+  firstRandomNum;
 
 })
+console.log(num)
+
+// get elements necessary to buid our game
+// create functions :
+// lancer:
 
 
-  
+// code to change the im dice :
