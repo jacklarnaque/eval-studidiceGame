960750433e9873bd3d1cdcd091ac20a993355c35
jacklarnaque <grousset1065@live.fr>
2022-07-06 19:03:32 +0100

    Co-authored-by: TBDC <ThomasBDC@users.noreply.github.com>

--- scriptGamePage.js
+++ scriptGamePage.js
@@ -1,29 +1,32 @@
-setTimeout(function () {
-    const diceLoader = document.getElementById('centering');
-    diceLoader.hidden = "true";
-}, 1000);
+
+// the code below is to get the players names from the prompt of the pagePlay.html:
+const urlParams = new URLSearchParams(window.location.search);
+const getQueryParams = (params, url) => {
+    let href = url;
+    // this is an expression to get query strings
+    let regexp = new RegExp( '[?&]' + params + '=([^&#]*)', 'i' );
+    let qString = regexp.exec(href);
+    return qString ? qString[1] : null;
+  };
+  
+  let player1 = getQueryParams('player1', window.location);
+  let player2 = getQueryParams('player2', window.location);
+
+//the code below is to display the  global users scores
+  document.getElementById('score1').innerHTML = `score global ${player1} = `
+  document.getElementById('score2').innerHTML = `score global ${player2} = `
+
+
+//we begin the algo for the dice game
 // function to pick a random number betwin 1 to 6
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
-//insert in the dom element via js to display the score :
-
-/* let score1P = document.createElement('p');
-score1P.innerHTML = ${result};
-let score1 = document.getElementById('score1');
-score1.appendChild(score1P); */
-
-//algo dices game //
-let throwDices = document.getElementById("lancer")
-let playerTurnDiceButton = document.getElementById("throw")
+  // we now get the 
+  // we need to get the variables necessary :
+  let roll = document.getElementById()
 
 
-playerTurnDiceButton.addEventListener('click', () => {
-    let n = getRandomInt(1, 7)
 
-})
-
-
-  
--- scriptHomePage.js
+++ scriptHomePage.js
@@ -2,27 +2,29 @@
 // set the script to switch to the game page straight after the both prompt done(players 1&2)
 // has tro be responsive 
 // start the game page with algo
+
 setTimeout(function () {
     const diceLoader = document.getElementById('centering');
     diceLoader.hidden = "true";
-}, 2000);
+}, 1000);
+
+
 
 // Get the players ID's :
 const buttonPlayer1 = document.getElementById('promptPlayer1');
 const buttonPlayer2 = document.getElementById('promptPlayer2');
 const startButton = document.getElementById('startButton');
 const buttonAnnonymous = document.getElementById('annonymousGame');
-export{buttonPlayer1, buttonPlayer2}
-
-
-startButton.hidden = true;
+// le ?  est la fin de lurl  et debut de  definition of the parameters of url
+/* startButton.hidden = true; 
+*/
 startButton.addEventListener("click", () => {
-    window.location.href = 'pagePlay.html'
+    window.location.href = 'pagePlay.html?player1=' + answer + '&player2=' + answer2;
 })
+// ensuite on recupere les varibales creer dans l'url avec une methode spécifique
 buttonAnnonymous.addEventListener("click", () => {
-    window.location.href = 'pagePlay.html'
+    window.location.href = 'pagePlay.html';
 })
-
 
 
 /* 
@@ -48,23 +50,29 @@ buttonAnnonymous.addEventListener("click", () => {
  */
 
 
+let answer = '';
+let answer2 = '';
+
 buttonPlayer1.addEventListener("click", () => {
-    let answer = prompt("entrez le nom du joueurs 1")
+    answer = prompt("entrez le nom du joueurs 1")
     if (answer.length > 10) {
         alert('Saisie incorrecte, le nom du joueur ne doit pas dépassé 10 caractères!');
     } else {
-        buttonPlayer1.innerHTML = answer
+        buttonPlayer1.innerHTML = answer;
         buttonAnnonymous.style.display = 'none';
     }
 });
-
-buttonPlayer2.addEventListener("click", () => {
-    let answer2 = prompt("entrez le nom du joueurs 2")
+let joueur2 = buttonPlayer2.addEventListener("click", () => {
+    answer2 = prompt("entrez le nom du joueurs 2")
     if ((answer2.length > 10)) {
         alert('Saisie incorrecte, le nom du joueur ne doit pas dépassé 10 caractères!');
     } else {
         buttonPlayer2.innerHTML = answer2;
         buttonAnnonymous.style.display = 'none';
         startButton.hidden = false;
+        return answer2
     }
 })
+
+
+
